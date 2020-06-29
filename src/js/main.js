5f0a39f319e79af0cc091cf27215d20b23681947
Emmanuel <rojaz813@gmail.com>
2020-06-30 02:14:22 +0300

    Making notes and todo tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 const themeToggle = document.getElementById('themeToggle')
 
 themeToggle.onchange = () => {
-    if (this.checked === false) {
+    if (themeToggle.checked === false) {
         document.documentElement.classList.remove('dark-theme')
-        document.querySelector("meta[name=theme-color]").setAttribute("content", "#fff");
+        document.querySelector("meta[name=theme-color]").setAttribute("content", "#fff")
+    } else {
+        document.documentElement.classList.toggle('dark-theme')
+        document.querySelector("meta[name=theme-color]").setAttribute("content", "#333")
     }
-
-    document.documentElement.classList.toggle('dark-theme')
-    document.querySelector("meta[name=theme-color]").setAttribute("content", "#333");
 }
 
 function openTab(evt, tabId) {
